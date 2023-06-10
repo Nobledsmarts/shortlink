e765233af3d7e77656d25ae78476830aec6d153e
nobledsmarts <nobledsmarts@gmail.com>
2023-06-10 23:07:12 +0100

    uninstalled tailwindcss

--- app/theme.ts
+++ app/theme.ts
@@ -1,9 +1,10 @@
 import { extendTheme } from '@chakra-ui/react'
 
 const colors = {
-    brand: {
+    transparent: 'transparent', 
+    // brand: {
 
-    }
+    // }
 }
 
 export const theme = extendTheme({ colors });
