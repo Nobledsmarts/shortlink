fadf746a33a186e4d877409657ee9478d653bd53
nobledsmarts <nobledsmarts@gmail.com>
2023-06-11 23:54:29 +0100

    typeface fix

--- app/page.tsx
+++ app/page.tsx
@@ -8,18 +8,19 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <Nav />
-      <HStack justifyContent={'space-between'} color={'aliceblue'} bg={'#13889f'} minHeight={500}>
-        <Flex px={'5rem'} flexDirection={'column'} gap={'1.5rem'} flexBasis={'60%'} h={'500'} justifyContent={'flex-start'} alignItems={'left'} bg={''}>
-          <Heading px={'0'} bgClip={'text'} color={'white'} as={'h1'} fontSize={'5xl'} mt={'100px'} fontFamily={'lexend'} bgGradient={'linear-gradient(to right, #121FCF 0%, #CF1512 100%)'}>The Shorter, The Better!</Heading>
-          <Text fontSize={'xl'} fontFamily={'montserrat'} width={'lg'}> 
+      <HStack position={'relative'} justifyContent={'space-between'} color={'aliceblue'} bg={'#13889f'} minHeight={450}>
+        <Flex px={'5rem'} flexDirection={'column'} gap={'1.5rem'} flexBasis={'60%'} h={'450'} justifyContent={'flex-start'} alignItems={'left'} bg={''}>
+          <Heading px={'0'} bgClip={'text'} color={'white'} as={'h1'} fontSize={'5xl'} mt={'100px'} fontFamily={'rubik'} fontWeight={'700'} bgGradient={'linear-gradient(to right, #121FCF 0%, #CF1512 100%)'}>The Shorter, The Better!</Heading>
+          <Text fontSize={'xl'} fontFamily={'raleway'} width={'lg'}> 
             ShortLink is a custom short link personalization tool that enables you to target, engage and helps you drive more customers.
           </Text>
+
           <Box>
             <Button  py={'1.3rem'} px={'3rem'} color={'#000'} bg={'aliceblue'} borderRadius={'2rem'}> Get started </Button>
           </Box>
         </Flex>
         <Flex>
-          <Image width={500} height={500} src={'/img2.png'} alt='' />
+          <Image width={450} height={450} src={'/img2.png'} alt='' />
         </Flex>
         <Box borderRadius={'50%'} position={'absolute'} left={'-40px'} zIndex={1} top={'100px'} width={'100'} overflow={'hidden'} border={'0px'} py={'0px'} height={'100px'}>
           {/* <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100">
@@ -33,10 +34,17 @@ export default function Home() {
           {/* <DottedLine/> */}
           <Image style={{zoom : 1}} color='#e50000'  src={'/26432.svg'} alt='' width={'100'} height={'100'}/>
         </Box>
-        <Box  position={'absolute'} left={'0px'} zIndex={1} bottom={'-50px'} border={'0px'} width={'100%'}  py={'0px'} height={'100px'}>
+        <Box  position={'absolute'} left={'0px'} zIndex={-1} bottom={'-50px'} border={'0px'} width={'100%'}  py={'0px'} height={'100px'}>
         
           <ChackraImage objectFit={'cover'} style={{zoom : 1}} color='#e50000'  src={'/wave2.svg'} alt='' width={'100%'} height={'250px'}/>
         </Box>
+      </HStack>
+      <HStack mt={'5rem'} justifyContent={'center'}>
+        <Flex justifyContent={'center'}>
+          <Heading px={'0'} bgClip={'text'} color={'cyan.900'} as={'h1'} fontSize={'5xl'} mt={'10px'} fontFamily={'rubik'} fontWeight={'500'} bgGradien={'linear-gradient(to right, #121FCF 0%, #CF1512 100%)'}>
+            What would you like to create?
+          </Heading>
+        </Flex>
       </HStack>
     </main>
   )
--- app/theme.ts
+++ app/theme.ts
@@ -2,33 +2,33 @@ import { extendTheme } from '@chakra-ui/react'
 import { Goldman, Rubik, Oswald, Raleway, Fira_Sans_Condensed, Lexend, Montserrat } from 'next/font/google';
 
 const oswald = Oswald({
-    weight : '400',
+    weight : ['200', '300', '400', '500', '600', '700'],
     subsets : ['latin']
 });
 
 const montserrat = Montserrat({
-    weight : '500',
+    weight : ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
     subsets : ['latin']
 });
 
 const raleway = Raleway({
-    weight : '400',
+    weight : ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
     subsets : ['latin']
 });
 const goldman = Goldman({
-    weight : '400',
+    weight : ['400', '700'],
     subsets : ['latin']
 });
 const rubik = Rubik({
-    weight : '400',
+    weight : ['300', '400', '500', '600', '700', '800', '900'],
     subsets : ['latin']
 });
 const lexend = Lexend({
-    weight : '400',
+    weight : ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
     subsets : ['latin']
 });
 const fira_condensed = Fira_Sans_Condensed({
-    weight : '400',
+    weight : ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
     subsets : ['latin']
 })
 
